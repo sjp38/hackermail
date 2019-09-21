e875171f98fa1319885b61ccccf9505f6a48cdb1
SeongJae Park <sj38.park@gmail.com>
2019-09-21 10:45:24 +0200

    _indent_patches: Add comments for better readability
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- _indent_patches.py
+++ _indent_patches.py
@@ -6,8 +6,13 @@ for line in sys.stdin:
     # patch lines start with "[PATCH"
     line = line.strip()
     fields = line.split()
-    if (fields[0] == "[PATCH]" or fields[1][0:2] == "0/" or
-            fields[1][-1] == "]" or fields[2][0:2] == "0/"):
+    if (fields[0] == "[PATCH]" or       # Single patch
+            fields[1][-1] == "]" or     # Versioned single patch,
+                                        #  e.g., [PATCH v2]
+            fields[1][0:2] == "0/" or   # First patch in series
+                                        #  e.g., [PATCH 0/3]
+            fields[2][0:2] == "0/"):    # First patch in versioned series
+                                        #  e.g., [PATCH v2 0/3]
         print(line)
         continue
     print("\t", line)
