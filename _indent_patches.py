885a9a76bcfbf0865c38027d6cd6b30e418ced9c
SeongJae Park <sj38.park@gmail.com>
2019-09-22 10:36:35 +0200

    _index_patches: Clean up comment
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- _indent_patches.py
+++ _indent_patches.py
@@ -3,9 +3,10 @@
 import sys
 
 for line in sys.stdin:
-    # patch lines start with "[PATCH"
+    # Each line should start with a subject of a patch mail
+    #       [PATCH], [PATCH v2], [PATCH 1/3], [PATCH v2 0/2], ...
     line = line.strip()
-    fields = line.split(']')[0][1:].split() # e.g., PATCH, PATCH v2, PATCH 1/3
+    fields = line.split(']')[0][1:].split()
     series_idx = fields[-1].split('/')[0]
     if series_idx.isdigit() and int(series_idx) != 0:
         line = "\t" + line
