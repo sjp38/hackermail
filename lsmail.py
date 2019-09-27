a9a1d39c76228fffe486a56849485ffab0361d94
SeongJae Park <sj38.park@gmail.com>
2019-09-27 20:22:29 +0200

    Add a script for entire mail listing
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

---
+++ lsmail.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import argparse
+import subprocess
+
+parser = argparse.ArgumentParser()
+parser.add_argument('since', metavar='since', type=str, nargs=1,
+        help='Show mails more recent than a specific date.')
+args = parser.parse_args()
+since = args.since
+
+cmd = ("git log --reverse".split() +
+        ['--date=iso-strict', '--pretty=%h %ad %s', "--since=%s" % since])
+
+for line in subprocess.run(cmd, stdout=subprocess.PIPE).stdout.decode(
+        'utf-8').strip().split('\n'):
+    fields = line.split()
+    gitid = fields[0]
+    date = fields[1]
+    subject = ' '.join(fields[2:])
+
+#    print(line)
+    print("commit %s\tdate %s\tsubject %s" % (gitid, date, subject))
