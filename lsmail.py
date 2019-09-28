c81d21331a716cd4dc8b217e2f85b59cf369cd7d
SeongJae Park <sj38.park@gmail.com>
2019-09-28 07:14:24 +0200

    lsmail: Support types filtering
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- lsmail.py
+++ lsmail.py
@@ -8,8 +8,14 @@ INDENT = ' ' * 4
 parser = argparse.ArgumentParser()
 parser.add_argument('since', metavar='since', type=str, nargs=1,
         help='Show mails more recent than a specific date.')
+parser.add_argument('--types', metavar='types', type=str, nargs='+',
+        help='Type of mails (patch, rfc, etc, all) to show.')
 args = parser.parse_args()
 since = args.since
+types = args.types
+
+if not types or 'all' in types:
+    types = ['patch', 'rfc', 'etc']
 
 cmd = ("git log".split() +
         ['--date=iso-strict', '--pretty=%h %ad %s', "--since=%s" % since])
@@ -26,6 +32,8 @@ for line in subprocess.run(cmd, stdout=subprocess.PIPE).stdout.decode(
 
     is_reply = False
     if subject_fields[0] in ['re:', 'RE:', 'Re:']:
+        if not 'etc' in types:
+            continue
         is_reply = True
         original_subject = ' '.join(subject_fields[1:])
         if original_subject in duplicate_re_map:
@@ -37,8 +45,14 @@ for line in subprocess.run(cmd, stdout=subprocess.PIPE).stdout.decode(
         tag = subject[1:].split(']')[0].strip()
         tag_fields = tag.split()
         if tag_fields[0] in ['PATCH', 'RFC']:
+            if tag_fields[0] == 'PATCH' and not 'patch' in types:
+                continue
+            if tag_fields[0] == 'RFC' and not 'rfc' in types:
+                continue
             series = tag_fields[-1].split('/')[0]
             if series.isdigit() and int(series) != 0:
                 subject = INDENT + subject
+        elif not 'etc' in types:
+            continue
 
     print("%s [%s] %s" % (gitid, date, subject))
