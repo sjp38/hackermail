4d51091e22940f361782fd43a43caefd874fb6cc
SeongJae Park <sj38.park@gmail.com>
2019-10-01 21:26:08 +0200

    _url: Add git directory argument
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- _url.sh
+++ _url.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
-if [ $# -ne 1 ]
+if [ $# -lt 1 ]
 then
-	echo "Usage: $0 <git hash>"
+	echo "Usage: $0 <git hash> [git dir]"
 	exit 1
 fi
 
 HASH=$1
 
-MSGID=$(git show $HASH:m | grep -e "^Message-Id: " | \
+if [ $# -gt 1 ]
+then
+	GDIR=$2
+fi
+
+if [ -z "$GDIR" ]
+then
+	GDIR="./.git"
+fi
+
+MSGID=$(git --git-dir=$GDIR show $HASH:m | grep -e "^Message-ID: " | \
 	awk -e '{print substr($2, 2, length($2) - 2)}')
 echo https://lkml.kernel.org/r/$MSGID
