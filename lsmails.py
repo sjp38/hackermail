a6035b5b22b359a4fe5ae772852b01d447db0b79
SeongJae Park <sj38.park@gmail.com>
2019-10-08 20:38:28 +0200

    lsmails: Enclose main code in the main condition if statement
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- lsmails.py
+++ lsmails.py
@@ -99,74 +99,76 @@ def show_mails(mails):
         line = "%s%s%s" % (line_prefix, indent, mail.subject)
         pr_line_wrap(line, len(line_prefix) + len(indent), nr_cols_in_line)
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--since', metavar='since', type=str,
-        help='Show mails more recent than a specific date.')
-parser.add_argument('--tags', metavar='tag', type=str, nargs='+',
-        help='Show mails having the tags (e.g., patch, rfc, reply, ...) only.')
-parser.add_argument('--filters', metavar='tag', type=str, nargs='+',
-        help='Filter out mails having the tags.')
-parser.add_argument('--mdir', metavar='mdir', type=str,
-        help='Directory containing the mail data.')
-parser.add_argument('--cols', metavar='cols', type=int,
-        help='Number of columns for each line.')
-parser.add_argument('--gitid', action='store_true',
-        help='Print git id of each mail')
-parser.add_argument('content', metavar='idx', type=int, nargs='?',
-        help='Show content of specific mail.')
-parser.add_argument('--lore', action='store_true',
-        help='Print lore link for the <content> mail.')
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--since', metavar='since', type=str,
+            help='Show mails more recent than a specific date.')
+    parser.add_argument('--tags', metavar='tag', type=str, nargs='+',
+            help='Show mails having the tags (e.g., patch, rfc, reply, ...) only.')
+    parser.add_argument('--filters', metavar='tag', type=str, nargs='+',
+            help='Filter out mails having the tags.')
+    parser.add_argument('--mdir', metavar='mdir', type=str,
+            help='Directory containing the mail data.')
+    parser.add_argument('--cols', metavar='cols', type=int,
+            help='Number of columns for each line.')
+    parser.add_argument('--gitid', action='store_true',
+            help='Print git id of each mail')
+    parser.add_argument('content', metavar='idx', type=int, nargs='?',
+            help='Show content of specific mail.')
+    parser.add_argument('--lore', action='store_true',
+            help='Print lore link for the <content> mail.')
 
-args = parser.parse_args()
-since = args.since
-tags = args.tags
-filters = args.filters
-mdir = args.mdir
-nr_cols_in_line = args.cols
-pr_git_id = args.gitid
-idx_of_mail = args.content
-show_lore_link = args.lore
+    args = parser.parse_args()
+    since = args.since
+    tags = args.tags
+    filters = args.filters
+    mdir = args.mdir
+    nr_cols_in_line = args.cols
+    pr_git_id = args.gitid
+    idx_of_mail = args.content
+    show_lore_link = args.lore
 
-if show_lore_link and idx_of_mail == None:
-    print("--lore option works with content argument only.\n")
-    parser.print_help()
-    exit(1)
+    if show_lore_link and idx_of_mail == None:
+        print("--lore option works with content argument only.\n")
+        parser.print_help()
+        exit(1)
 
-if not since:
-    since_date = datetime.datetime.now() - datetime.timedelta(days=3)
-    since = "%s-%s-%s" % (since_date.year, since_date.month, since_date.day)
+    if not since:
+        since_date = datetime.datetime.now() - datetime.timedelta(days=3)
+        since = "%s-%s-%s" % (since_date.year, since_date.month,
+                since_date.day)
 
-if not mdir:
-    mdir = "./.git"
+    if not mdir:
+        mdir = "./.git"
 
-if not nr_cols_in_line:
-    nr_cols_in_line = 130
+    if not nr_cols_in_line:
+        nr_cols_in_line = 130
 
-cmd = ["git", "--git-dir=%s" % mdir, "log",
-        '--date=iso-strict', '--pretty=%h %ad %s', "--since=%s" % since]
+    cmd = ["git", "--git-dir=%s" % mdir, "log",
+            '--date=iso-strict', '--pretty=%h %ad %s', "--since=%s" % since]
 
-mails_to_show = []
-duplicate_re_map = {}
-for line in subprocess.run(cmd, stdout=subprocess.PIPE).stdout.decode(
-        'utf-8').strip().split('\n'):
-    fields = line.split()
-    if len(fields) < 3:
-        continue
-    mail = Mail(fields[0], fields[1], fields[2:])
-
-    if not valid_to_show(mail):
-        continue
-
-    # Shows only latest reply for given mail
-    if mail.tags and 'reply' in mail.tags:
-        if mail.orig_subject in duplicate_re_map:
+    mails_to_show = []
+    duplicate_re_map = {}
+    for line in subprocess.run(cmd, stdout=subprocess.PIPE).stdout.decode(
+            'utf-8').strip().split('\n'):
+        fields = line.split()
+        if len(fields) < 3:
             continue
-        duplicate_re_map[mail.orig_subject] = True
+        mail = Mail(fields[0], fields[1], fields[2:])
 
-    mails_to_show.append(mail)
+        if not valid_to_show(mail):
+            continue
 
-mails_to_show.reverse()
-if idx_of_mail != None:
-    show_mail(mails_to_show[idx_of_mail])
-else:
-    show_mails(mails_to_show)
+        # Shows only latest reply for given mail
+        if mail.tags and 'reply' in mail.tags:
+            if mail.orig_subject in duplicate_re_map:
+                continue
+            duplicate_re_map[mail.orig_subject] = True
+
+        mails_to_show.append(mail)
+
+    mails_to_show.reverse()
+    if idx_of_mail != None:
+        show_mail(mails_to_show[idx_of_mail])
+    else:
+        show_mails(mails_to_show)
