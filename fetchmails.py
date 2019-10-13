08f0c14052caf4334664d085365c39dc0f3d0e3f
SeongJae Park <sj38.park@gmail.com>
2019-10-13 13:07:42 +0200

    fetchmails: Implement manifest parsing feature
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- fetchmails.py
+++ fetchmails.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
+import json
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--manifest', '-m', type=str,
@@ -10,7 +11,14 @@ parser.add_argument('lists', type=str, nargs='+',
         help='Name of the mailing list')
 
 args = parser.parse_args()
-manifest = args.manifest
+manifest_file = args.manifest
 mail_lists = args.lists
 
-print(manifest, mail_lists)
+with open(manifest_file) as f:
+    manifest = json.load(f)
+
+site = manifest['site']
+for path in manifest:
+    for mlist in mail_lists:
+        if path.startswith('/%s/' % mlist):
+            print('%s%s' % (site, path))
