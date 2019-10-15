abc7fff3ee62232589b0de73f4560a8296724bd3
SeongJae Park <sj38.park@gmail.com>
2019-10-15 20:53:29 +0200

    lsmails: Clean up show_mails() code
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- lsmails.py
+++ lsmails.py
@@ -93,15 +93,17 @@ def show_mails(mails):
         # date: <YYYY-MM-DD>T<HH>:<MM>:<SS>+<UTC offset>
         #       e.g., 2019-09-30T09:57:38+08:00
         date = '/'.join(mail.date.split('T')[0].split('-')[1:])
-        prefixes = ["[%04d]" % idx, date]
+        prefix_fields = ["[%04d]" % idx, date]
         if pr_git_id:
-            prefixes.append(mail.gitid)
-        line_prefix = ' '.join(prefixes) + ' '
-        line = "%s%s%s" % (line_prefix, indent, mail.subject)
+            prefix_fields.append(mail.gitid)
+        prefix_fields.append(indent)
+        prefix = ' '.join(prefix_fields)
+
+        line = mail.subject
         if nr_news_in_thr[mail.orig_subject] > 1:
             line += " (%d more msgs) " % (nr_news_in_thr[mail.orig_subject] - 1)
 
-        pr_line_wrap(line, len(line_prefix) + len(indent), nr_cols_in_line)
+        pr_line_wrap(prefix + line, len(prefix), nr_cols_in_line)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
