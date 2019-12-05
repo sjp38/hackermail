5c6869a261ae1b2c903fde8e4a699f9a206bcde8
SeongJae Park <sj38.park@gmail.com>
2019-12-05 19:14:28 +0100

    __format_reply: Make usable from other modules
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- __format_reply.py
+++ __format_reply.py
@@ -2,26 +2,30 @@
 
 import sys
 
-head_fields = {}
+def format_reply(lines):
+    head_fields = {}
 
-in_header = True
-for line in sys.stdin:
-    if in_header:
-        if line and line[0] in [' ', '\t'] and key:
-            head_fields[key] += ' %s' % line.strip()
+    in_header = True
+    for line in lines:
+        if in_header:
+            if line and line[0] in [' ', '\t'] and key:
+                head_fields[key] += ' %s' % line.strip()
+                continue
+            line = line.strip()
+            key = line.split(':')[0].lower()
+            if key:
+                head_fields[key] = line[len(key) + 2:]
+            elif line == '':
+                in_header = False
+                print("Subject: Re: %s" % head_fields['subject'])
+                print("In-Reply-To: %s" % head_fields['message-id'])
+                print("Cc: %s" % head_fields['cc'])
+                print("To: %s" % head_fields['from'])
+                print("")
+                print("On %s %s wrote:\n" % (head_fields['date'], head_fields['from']))
             continue
         line = line.strip()
-        key = line.split(':')[0].lower()
-        if key:
-            head_fields[key] = line[len(key) + 2:]
-        elif line == '':
-            in_header = False
-            print("Subject: Re: %s" % head_fields['subject'])
-            print("In-Reply-To: %s" % head_fields['message-id'])
-            print("Cc: %s" % head_fields['cc'])
-            print("To: %s" % head_fields['from'])
-            print("")
-            print("On %s %s wrote:\n" % (head_fields['date'], head_fields['from']))
-        continue
-    line = line.strip()
-    print(">%s" % line)
+        print(">%s" % line)
+
+if __name__ == '__main__':
+    format_reply(sys.stdin)
