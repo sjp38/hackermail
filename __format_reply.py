67cae5b5d9f825bb0eb045663ea49497db7929c0
SeongJae Park <sj38.park@gmail.com>
2019-12-05 19:08:25 +0100

    __format_reply: Receive input from stdin
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- __format_reply.py
+++ __format_reply.py
@@ -2,30 +2,26 @@
 
 import sys
 
-if len(sys.argv) != 2:
-    print("Usage: %s <original plain mbox file>")
-
 head_fields = {}
 
 in_header = True
-with open(sys.argv[1], 'r') as f:
-        for line in f:
-            if in_header:
-                if line and line[0] in [' ', '\t'] and key:
-                    head_fields[key] += ' %s' % line.strip()
-                    continue
-                line = line.strip()
-                key = line.split(':')[0].lower()
-                if key:
-                    head_fields[key] = line[len(key) + 2:]
-                elif line == '':
-                    in_header = False
-                    print("Subject: Re: %s" % head_fields['subject'])
-                    print("In-Reply-To: %s" % head_fields['message-id'])
-                    print("Cc: %s" % head_fields['cc'])
-                    print("To: %s" % head_fields['from'])
-                    print("")
-                    print("On %s %s wrote:\n" % (head_fields['date'], head_fields['from']))
-                continue
-            line = line.strip()
-            print(">%s" % line)
+for line in sys.stdin:
+    if in_header:
+        if line and line[0] in [' ', '\t'] and key:
+            head_fields[key] += ' %s' % line.strip()
+            continue
+        line = line.strip()
+        key = line.split(':')[0].lower()
+        if key:
+            head_fields[key] = line[len(key) + 2:]
+        elif line == '':
+            in_header = False
+            print("Subject: Re: %s" % head_fields['subject'])
+            print("In-Reply-To: %s" % head_fields['message-id'])
+            print("Cc: %s" % head_fields['cc'])
+            print("To: %s" % head_fields['from'])
+            print("")
+            print("On %s %s wrote:\n" % (head_fields['date'], head_fields['from']))
+        continue
+    line = line.strip()
+    print(">%s" % line)
