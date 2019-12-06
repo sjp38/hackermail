59af578a206629053971c25d227b6244a80f7bdc
SeongJae Park <sj38.park@gmail.com>
2019-12-06 18:36:11 +0100

    __format_reply: Write headers if the key exists only
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- __format_reply.py
+++ __format_reply.py
@@ -17,10 +17,14 @@ def format_reply(lines):
                 head_fields[key] = line[len(key) + 2:]
             elif line == '':
                 in_header = False
-                print("Subject: Re: %s" % head_fields['subject'])
-                print("In-Reply-To: %s" % head_fields['message-id'])
-                print("Cc: %s" % head_fields['cc'])
-                print("To: %s" % head_fields['from'])
+                if 'subject' in head_fields:
+                    print("Subject: Re: %s" % head_fields['subject'])
+                if 'message-id' in head_fields:
+                    print("In-Reply-To: %s" % head_fields['message-id'])
+                if 'cc' in head_fields:
+                    print("Cc: %s" % head_fields['cc'])
+                if 'from' in head_fields:
+                    print("To: %s" % head_fields['from'])
                 print("")
                 print("On %s %s wrote:\n" % (head_fields['date'], head_fields['from']))
             continue
