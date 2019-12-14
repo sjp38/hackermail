da0c0f89084ed33295dc4f638787dfe7ef7440fb
SeongJae Park <sj38.park@gmail.com>
2019-12-14 23:06:04 +0100

    test.sh: Add --hkml_dir test
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- test.sh
+++ test.sh
@@ -6,3 +6,14 @@ $HKML ls linux-mm || exit 1
 $HKML ls linux-mm 2 || exit 2
 $HKML format_reply linux-mm 2 || exit 3
 $HKML fetch linux-mm workflows || exit 4
+
+mv .hkm .hkml
+
+$HKML --hkml_dir .hkml ls linux-mm || exit 1
+$HKML --hkml_dir .hkml ls linux-mm 2 || exit 2
+$HKML --hkml_dir .hkml format_reply linux-mm 2 || exit 3
+$HKML --hkml_dir .hkml fetch linux-mm workflows || exit 4
+
+mv .hkml .hkm
+
+echo "SUCCESS"
