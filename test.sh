6dc402805929aa399a6269c5e730a475cf0434f9
SeongJae Park <sj38.park@gmail.com>
2019-12-25 10:29:01 +0100

    test: Add tests for non-mailing list option given fetch
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- test.sh
+++ test.sh
@@ -6,6 +6,7 @@ $HKML ls linux-mm || exit 1
 $HKML ls linux-mm 2 || exit 2
 $HKML format_reply linux-mm 2 || exit 3
 $HKML fetch linux-mm workflows || exit 4
+$HKML fetch || exit 5
 
 mv .hkm .hkml
 
@@ -13,6 +14,7 @@ $HKML --hkml_dir .hkml ls linux-mm || exit 1
 $HKML --hkml_dir .hkml ls linux-mm 2 || exit 2
 $HKML --hkml_dir .hkml format_reply linux-mm 2 || exit 3
 $HKML --hkml_dir .hkml fetch linux-mm workflows || exit 4
+$HKML --hkml_dir .hkml fetch || exit 5
 
 mv .hkml .hkm
 
