46be525e3acc2906b4040ad1b8229871bc4a180d
SeongJae Park <sj38.park@gmail.com>
2020-01-01 09:31:29 +0100

    test.sh: Skip user interaction
    Because lsmail do connect output to less, it is annoying to press enter
    or 'q' to quit from the lsmail's output.  This commit makes it to be
    skipped by redirecting the output to a file.
    
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- test.sh
+++ test.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
 HKML=./hkml
+OUT=./test_out
 
-$HKML ls linux-mm || exit 1
-$HKML ls linux-mm 2 || exit 2
-$HKML format_reply linux-mm 2 || exit 3
-$HKML fetch linux-mm workflows || exit 4
-$HKML fetch || exit 5
+$HKML ls linux-mm > $OUT || exit 1
+$HKML ls linux-mm 2 >> $OUT || exit 2
+$HKML format_reply linux-mm 2 >> $OUT || exit 3
+$HKML fetch linux-mm workflows >> $OUT || exit 4
+$HKML fetch >> $OUT || exit 5
 
 mv .hkm .hkml
 
-$HKML --hkml_dir .hkml ls linux-mm || exit 1
-$HKML --hkml_dir .hkml ls linux-mm 2 || exit 2
-$HKML --hkml_dir .hkml format_reply linux-mm 2 || exit 3
-$HKML --hkml_dir .hkml fetch linux-mm workflows || exit 4
-$HKML --hkml_dir .hkml fetch || exit 5
+$HKML --hkml_dir .hkml ls linux-mm >> $OUT || exit 1
+$HKML --hkml_dir .hkml ls linux-mm 2 >> $OUT || exit 2
+$HKML --hkml_dir .hkml format_reply linux-mm 2 >> $OUT || exit 3
+$HKML --hkml_dir .hkml fetch linux-mm workflows >> $OUT || exit 4
+$HKML --hkml_dir .hkml fetch >> $OUT || exit 5
 
 mv .hkml .hkm
 
+cat $OUT
 echo "SUCCESS"
