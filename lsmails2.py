273a3bbbbec62e684dba73c7c3924a32fe30313d
SeongJae Park <sj38.park@gmail.com>
2020-02-01 09:07:47 +0100

    lsmails2: Add Mail class
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- lsmails2.py
+++ lsmails2.py
@@ -1,3 +1,21 @@
 #!/usr/bin/env python3
 
+import _hkml
+
+class Mail:
+    fields = None
+    mbox = None
+
+    def __init__(self, mbox):
+        self.mbox = mbox
+        parsed = _hkml.parse_mbox(mbox)
+        self.fields = parsed['header']
+        self.fields['body'] = parsed['body']
+
+    def get_field(self, key):
+        return self.fields[key]
+
+    def set_fields(self, key, val):
+        self.fields[key] = val
+
 print('hello')
