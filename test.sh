f137c6c23f6680111c14efe461f5393471d30636
SeongJae Park <sj38.park@gmail.com>
2020-03-15 11:09:01 +0100

    test.sh: Update for new usages
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- test.sh
+++ test.sh
@@ -4,16 +4,16 @@ HKML=./hkml
 OUT=./test_out
 
 $HKML ls linux-mm > $OUT || exit 1
-$HKML ls linux-mm 2 >> $OUT || exit 2
-$HKML format_reply linux-mm 2 >> $OUT || exit 3
+$HKML ls linux-mm -i 2 >> $OUT || exit 2
+$HKML format_reply linux-mm -i 2 >> $OUT || exit 3
 $HKML fetch linux-mm workflows >> $OUT || exit 4
 $HKML fetch >> $OUT || exit 5
 
 mv .hkm .hkml
 
 $HKML --hkml_dir .hkml ls linux-mm >> $OUT || exit 1
-$HKML --hkml_dir .hkml ls linux-mm 2 >> $OUT || exit 2
-$HKML --hkml_dir .hkml format_reply linux-mm 2 >> $OUT || exit 3
+$HKML --hkml_dir .hkml ls linux-mm -i 2 >> $OUT || exit 2
+$HKML --hkml_dir .hkml format_reply linux-mm -i 2 >> $OUT || exit 3
 $HKML --hkml_dir .hkml fetch linux-mm workflows >> $OUT || exit 4
 $HKML --hkml_dir .hkml fetch >> $OUT || exit 5
 
