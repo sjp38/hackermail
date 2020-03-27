1d4bb015acf82d6c2fc018f2fee6e93f7ae30d03
SeongJae Park <sj38.park@gmail.com>
2020-03-27 06:49:22 +0100

    _hkml: Move fetchmail specific code to fetchmail
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- fetchmails.py
+++ fetchmails.py
@@ -6,6 +6,36 @@ import subprocess
 
 import _hkml
 
+def fetch_mail(manifest_file, mail_lists, quiet=False):
+    manifest = _hkml.get_manifest(manifest_file)
+    if not manifest:
+        print("Cannot open manifest file %s" % manifest_file)
+        exit(1)
+
+    site = manifest['site']
+    for mlist in mail_lists:
+        repo_paths = _hkml.mail_list_repo_paths(mlist, manifest)
+        local_paths = _hkml.mail_list_data_paths(mlist, manifest)
+        for idx, repo_path in enumerate(repo_paths):
+            git_url = '%s%s' % (site, repo_path)
+            local_path = local_paths[idx]
+            if not os.path.isdir(local_path):
+                cmd = 'git clone --mirror %s %s' % (git_url, local_path)
+            else:
+                cmd = 'git --git-dir=%s remote update' % local_path
+            if not quiet:
+                print(cmd)
+                subprocess.call(cmd.split())
+            else:
+                with open(os.devnull, 'w') as f:
+                    subprocess.call(cmd.split(), stdout=f)
+
+def fetched_mail_lists():
+    archive_dir = os.path.join(_hkml.get_hkml_dir(), 'archives')
+    mail_dirs = _hkml.cmd_lines_output(['ls', archive_dir])
+    return [x for x in mail_dirs if os.path.isdir(
+        os.path.join(archive_dir, x))]
+
 def set_argparser(parser):
     _hkml.set_manifest_mlist_options(parser, mlist_nargs='*')
     parser.add_argument('--quiet', '-q', default=False, action='store_true',
@@ -20,9 +50,9 @@ def main(args=None):
     mail_lists = args.mlist
     manifest_file = args.manifest
     if not mail_lists:
-        mail_lists = _hkml.fetched_mail_lists()
+        mail_lists = fetched_mail_lists()
     quiet = args.quiet
-    _hkml.fetch_mail(manifest_file, mail_lists, quiet)
+    fetch_mail(manifest_file, mail_lists, quiet)
 
 if __name__ == '__main__':
     main()
