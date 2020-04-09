11ed7ce82f930df454cee7f3c0d8a145ba2b0b73
SeongJae Park <sj38.park@gmail.com>
2020-04-09 06:41:26 +0200

    lsmails: Open mail if only one mail is selected
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- lsmails.py
+++ lsmails.py
@@ -94,6 +94,7 @@ def nr_replies_of(mail):
 
 def pr_mails_thread(mail, mail_idx, depth):
     global ls_range
+    global open_mail
 
     nr_printed = 1
 
@@ -103,6 +104,8 @@ def pr_mails_thread(mail, mail_idx, depth):
         suffix = ' (%d+ msgs)' % nr_replies
         nr_printed += nr_replies
 
+    if ls_range[1] == 1:
+        open_mail = True
     if mail_idx >= ls_range[0] and (ls_range[1] == -1 or
             mail_idx < ls_range[0] + ls_range[1]):
         pr_mail_subject(mail, depth, suffix, mail_idx)
