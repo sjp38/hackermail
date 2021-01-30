cc64ecafe3a4e1646ad07ba973b5812dccb5dd50
SeongJae Park <sj38.park@gmail.com>
2021-01-30 09:53:20 +0100

    format_mail: Support 'in-reply-to'
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- format_mail.py
+++ format_mail.py
@@ -24,7 +24,7 @@ def git_sendemail_valid_recipients(recipients):
     lines[-1] = lines[-1][:-1]
     return '\n'.join(lines)
 
-def format_mbox(subject, to, cc, body):
+def format_mbox(subject, in_reply_to, to, cc, body):
     if not subject:
         subject = '/* write subject here */'
     if not to:
@@ -32,6 +32,8 @@ def format_mbox(subject, to, cc, body):
     if not cc:
         cc = ['/* wrtite cc recipients here */']
     print('Subject: %s' % subject)
+    if in_reply_to:
+        print('In-Reply-To: %s' % in_reply_to)
     for addr in to:
         addr = git_sendemail_valid_recipients(addr)
         print('To: %s' % addr)
@@ -46,6 +48,8 @@ def format_mbox(subject, to, cc, body):
 def set_argparser(parser=None):
     parser.add_argument('--subject', metavar='<subject>', type=str,
             help='Subject of the mail.')
+    parser.add_argument('--in-reply-to', metavar='<message id>',
+            help='Add in-reply-to field in the mail header')
     parser.add_argument('--to', metavar='<email address>', nargs='+',
             help='recipients of the mail')
     parser.add_argument('--cc', metavar='<email address>', nargs='+',
@@ -59,7 +63,7 @@ def main(args=None):
         set_argparser(parser)
         args = parser.parse_args()
 
-    format_mbox(args.subject, args.to, args.cc, args.body)
+    format_mbox(args.subject, args.in_reply_to, args.to, args.cc, args.body)
 
 if __name__ == '__main__':
     main()
