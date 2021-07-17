88dca2ebf3469c809bd76618eb4b6f9da2311cf5
SeongJae Park <sj38.park@gmail.com>
2021-07-17 10:14:51 +0200

    manifest: Replace keyword with mailing list names
    This commit makes 'manifest' to receive exact mailing list names to
    print, instead of keywords.
    
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- manifest.py
+++ manifest.py
@@ -6,26 +6,35 @@ import _hkml
 
 def set_argparser(parser):
     _hkml.set_manifest_option(parser)
-    parser.add_argument('--keywords', metavar='<keyword>', nargs='+',
-            help='print manifest entries having key with this keywords')
+    parser.add_argument('--mlists', metavar='<mailing list name>', nargs='+',
+            help='print manifest entries for specific mailing lists')
 
-def pr_directory(directory, keywords, depth=0):
+def need_to_print(key, depth, mlists):
+    if depth > 0:
+        return True
+    if not mlists:
+        return True
+
+    # expected key: /linux-bluetooth/git/0.git
+    if key[-4:].strip() != '.git':
+        return False
+    fields = key.split('/')
+    if len(fields) != 4:
+        print(fields)
+        return False
+    return fields[1] in mlists
+
+def pr_directory(directory, mlists, depth=0):
     indent = ' ' * 4 * depth
     for key in directory:
-        found = False
-        if keywords:
-            for keyword in keywords:
-                if key.find(keyword) != -1:
-                    found = True
-                    break
-            if not found:
-                continue
+        if not need_to_print(key, depth, mlists):
+            continue
 
         val = directory[key]
 
         if type(val) == dict:
             print('%s%s: {' % (indent, key))
-            pr_directory(val, None, depth + 1)
+            pr_directory(val, mlists, depth + 1)
             print('%s}' % indent)
         else:
             print('%s%s: %s' % (indent, key, val))
@@ -36,7 +45,7 @@ def main(args=None):
         set_argparser(parser)
         args = parser.parse_args()
 
-    pr_directory(_hkml.get_manifest(args.manifest), args.keywords)
+    pr_directory(_hkml.get_manifest(args.manifest), args.mlists)
 
 if __name__ == '__main__':
     main()
