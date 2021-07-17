14abd60cbd18724fe2ad25e242ceba649e881f42
SeongJae Park <sj38.park@gmail.com>
2021-07-17 10:04:06 +0200

    manifest: Implement keyword-based printing
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- manifest.py
+++ manifest.py
@@ -6,15 +6,26 @@ import _hkml
 
 def set_argparser(parser):
     _hkml.set_manifest_option(parser)
+    parser.add_argument('--keywords', metavar='<keyword>', nargs='+',
+            help='print manifest entries having key with this keywords')
 
-def pr_directory(directory, depth=0):
+def pr_directory(directory, keywords, depth=0):
     indent = ' ' * 4 * depth
     for key in directory:
+        found = False
+        if keywords:
+            for keyword in keywords:
+                if key.find(keyword) != -1:
+                    found = True
+                    break
+            if not found:
+                continue
+
         val = directory[key]
 
         if type(val) == dict:
             print('%s%s: {' % (indent, key))
-            pr_directory(val, depth + 1)
+            pr_directory(val, None, depth + 1)
             print('%s}' % indent)
         else:
             print('%s%s: %s' % (indent, key, val))
@@ -25,7 +36,7 @@ def main(args=None):
         set_argparser(parser)
         args = parser.parse_args()
 
-    pr_directory(_hkml.get_manifest(args.manifest))
+    pr_directory(_hkml.get_manifest(args.manifest), args.keywords)
 
 if __name__ == '__main__':
     main()
