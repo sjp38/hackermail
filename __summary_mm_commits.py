c9a2dbd7c6f9badadefb71688d18489794efdade
SeongJae Park <sj38.park@gmail.com>
2021-10-18 08:03:51 +0200

    __summary_mm_commits: Support tags in removals
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- __summary_mm_commits.py
+++ __summary_mm_commits.py
@@ -16,18 +16,20 @@ def main():
         mails.append(mail)
 
     added = []
-    removed = []
+    removed = {}
     for mail in mails:
         tokens = mail.split()[2:]
         if len(tokens) < 9:
             continue
-        symbol = tokens[0]
+        tag = tokens[0]
         patch = tokens[1].split('.patch')[0]
-        action = tokens[2:6]
-        if action == ['added', 'to', '-mm', 'tree']:
+        action = ' '.join(tokens[2:6])
+        if tag == '+' and action == 'added to -mm tree':
             added.append(patch)
-        if action == ['removed' 'from', '-mm', 'tree']:
-            removed.append(patch)
+        if action == 'removed from -mm tree':
+            if not tag in removed:
+                removed[tag] = []
+            removed[tag].append(patch)
 
     print('added patches')
     print('-------------')
@@ -39,11 +41,15 @@ def main():
     print('removed patches')
     print('---------------')
     print()
-    for patch in removed:
-        print(patch)
+    for tag in removed:
+        for patch in removed[tag]:
+            print('%s %s' % (tag, patch))
 
+    nr_removes = 0
+    for tag in removed:
+        nr_removes += len(removed[tag])
     print()
-    print('%d added, %d removed' % (len(added), len(removed)))
+    print('%d added, %d removed' % (len(added), nr_removes))
 
 if __name__ == '__main__':
     main()
