67c58d79d919e106010b6c4caf8894e87214cca9
SeongJae Park <sj38.park@gmail.com>
2021-11-27 09:59:45 +0100

    mm_commits_changes: Add --help option
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- mm_commits_changes.sh
+++ mm_commits_changes.sh
@@ -1,11 +1,38 @@
 #!/bin/bash
 
-if [ $# -eq 1 ]
-then
-	duration_days=$1
-else
-	duration_days=7
-fi
+pr_usage()
+{
+	echo "Usage: $0 [OPTION]... [duration in days]"
+	echo
+	echo "OPTION"
+	echo "  -h, --help	Show this message"
+}
+
+function pr_usage_exit {
+	exit_code=$1
+	pr_usage
+	exit "$exit_code"
+}
+
+duration_days=7
+
+while [ $# -ne 0 ]
+do
+	case $1 in
+	"--help" | "-h")
+		pr_usage_exit 0
+		;;
+	*)
+		if [ $# -ne 1 ]
+		then
+			pr_usage_exit 1
+		fi
+		duration_days=$1
+		break
+		;;
+	esac
+done
+
 since=$(date --date="-$duration_days day" +%Y-%m-%d)
 
 bindir=$(dirname "$0")
