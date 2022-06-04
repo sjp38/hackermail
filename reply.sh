f328523e1aca183a2d08b5f97268478e26cbfc6d
SeongJae Park <sj38.park@gmail.com>
2022-06-04 20:38:39 +0000

    reply: Allow deleting the reply when not sending it
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- reply.sh
+++ reply.sh
@@ -12,6 +12,13 @@ echo
 read -rp "Will send above mail.  Okay? [y/N] " answer
 if [ ! "$answer" = "y" ]
 then
+	read -rp "Leave the reply message? [Y/n] " answer
+	if [ "$answer" = "n" ]
+	then
+		rm "$reply_file"
+	else
+		echo "your reply message is at $reply_file"
+	fi
 	exit
 fi
 
