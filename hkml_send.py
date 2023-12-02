251480cb7c4a028918bebc57f2367e5310fd17ec
SeongJae Park <sj38.park@gmail.com>
2023-12-02 18:46:46 +0000

    hkml_send: Support getting confirmation
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- hkml_send.py
+++ hkml_send.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
+import os
 import subprocess
 
 import _hkml
@@ -9,7 +10,18 @@ def set_argparser(parser=None):
     parser.add_argument('mbox_file', metavar='<mboxfile>',
             help='Mbox format file of the mail to send.')
 
-def send_mail(mboxfile):
+def send_mail(mboxfile, get_confirm=False):
+    if get_confirm:
+        with open(mboxfile, 'r') as f:
+            print(f.read())
+        answer = input('Will send above mail.  Okay? [y/N] ')
+        if answer.lower() != 'y':
+            answer = input('Leave the draft message? [Y/n] ')
+            if answer.lower() == 'n':
+                os.remove(mboxfile)
+            else:
+                print('The draft message is at %s' % mboxfile)
+            exit(0)
     _hkml.cmd_str_output(['git', 'send-email', mboxfile])
 
 def main(args=None):
@@ -18,7 +30,7 @@ def main(args=None):
         set_argparser(parser)
         args = parser.parse_args()
 
-    send_mail(args.mbox_file)
+    send_mail(args.mbox_file, get_confirm=False)
 
 if __name__ == '__main__':
     main()
