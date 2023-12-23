35a04347c9cf64dd56476fc65f7d1e8af91e19d3
SeongJae Park <sj38.park@gmail.com>
2023-12-23 10:39:00 -0800

    _hkml_cache: Print basic stat of cache file when executed
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- _hkml_cache.py
+++ _hkml_cache.py
@@ -39,3 +39,19 @@ def write_mails_cache_file():
     cache_path = os.path.join(_hkml.get_hkml_dir(), 'mails_cache')
     with open(cache_path, 'w') as f:
         json.dump(mails_cache, f, indent=4)
+
+def main():
+    cache_path = os.path.join(_hkml.get_hkml_dir(), 'mails_cache')
+    if not os.path.isfile(cache_path):
+        print('no cache exist')
+        exit(1)
+
+    cache_stat = os.stat(cache_path)
+    print('cache size: %.3f MiB' % (cache_stat.st_size / 1024 / 1024))
+
+    with open(cache_path, 'r') as f:
+        mails_cache = json.load(f)
+    print('%d mails in cache' % len(mails_cache))
+
+if __name__ == '__main__':
+    main()
