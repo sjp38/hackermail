30a7f0697a8e12797a9acb73a166d9b397149c62
SeongJae Park <sj38.park@gmail.com>
2023-12-29 12:52:16 -0800

    Implement 'open' command
    Implement intuitive 'open' command for reading single mail.
    
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

---
+++ hkml_open.py
@@ -0,0 +1,34 @@
+import json
+import os
+import subprocess
+import tempfile
+
+import _hkml
+import hkml_cache
+import hkml_list
+
+def set_argparser(parser):
+    parser.add_argument(
+            'mail_idx', metavar='<index>', type=int,
+            help='index of the mail to open and read')
+
+def main(args=None):
+    if not args:
+        parser = argparser.ArgumentParser()
+        set_Argparser(parser)
+        args = parser.parse_args()
+
+    with open(os.path.join(_hkml.get_hkml_dir(), 'mail_idx_to_cache_key'),
+              'r') as f:
+        key = json.load(f)['%d' % args.mail_idx]
+    mail = hkml_cache.get_mail(key=key)
+    if mail is None:
+        print('mail is not cached')
+        exit(1)
+
+    lines = []
+    hkml_list.pr_mail_content(mail, False, False, lines)
+    print('\n'.join(lines))
+
+if __name__ == 'main__':
+    main()
