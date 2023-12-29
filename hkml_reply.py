1c8d95e089034cc99bbdeff0aca98a0cc0722340
SeongJae Park <sj38.park@gmail.com>
2023-12-29 12:35:09 -0800

    Remove 'fmt_reply' command
    The command can be replaced by 'hkml reply' now.  Remove it.
    
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- hkml_reply.py
+++ hkml_reply.py
@@ -5,8 +5,29 @@ import tempfile
 
 import _hkml
 import hkml_cache
-import hkml_format_reply
 import hkml_send
+import hkml_write
+
+def format_reply(mail):
+    subject = mail.get_field('subject')
+    if subject and subject.split()[0].lower() != 're:':
+        subject = 'Re: %s' % subject
+
+    in_reply_to = mail.get_field('message-id')
+    cc = [x for x in [mail.get_field('to'), mail.get_field('cc')] if x]
+    to = [mail.get_field('from')]
+
+    body_lines = []
+    date = mail.get_field('date')
+    if date and to[0]:
+        body_lines.append('On %s %s wrote:' % (date, to[0]))
+        body_lines.append('')
+    body = mail.get_field('body')
+    for line in body.split('\n'):
+        body_lines.append('> %s' % line)
+    body = '\n'.join(body_lines)
+
+    return hkml_write.format_mbox(subject, in_reply_to, to, cc, body)
 
 def set_argparser(parser):
     parser.add_argument(
@@ -29,7 +50,7 @@ def main(args=None):
     if mail is None:
         print('mail is not cached')
         exit(1)
-    reply_mbox_str = hkml_format_reply.format_reply(mail)
+    reply_mbox_str = format_reply(mail)
     if args.format_only:
         print(reply_mbox_str)
         return
