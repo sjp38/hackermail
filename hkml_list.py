21fc146c650c7e8c3239a1eaf962a763cdc1e206
SeongJae Park <sj38.park@gmail.com>
2023-12-30 11:32:35 -0800

    hkml_list: Check incomplete mbox string input with clipboard
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- hkml_list.py
+++ hkml_list.py
@@ -335,7 +335,11 @@ def get_mails(
         return [m for m in mails if m is not None]
     if source == 'clipboard':
         mbox_str = _hkml.cmd_str_output(['xclip', '-o', '-sel', 'clip'])
-        return [_hkml.Mail(mbox=mbox_str)]
+        mail = _hkml.Mail(mbox=mbox_str)
+        if mail.broken():
+            print('clipboard is not complete mbox string')
+            return []
+        return [mail]
     elif os.path.isfile(source):
         return _hkml.read_mbox_file(source)
 
