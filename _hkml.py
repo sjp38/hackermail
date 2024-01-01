sjp38/hackermail

commit ebdb6742def6efeccf974221332c05b98ddc884c

_hkml: Replace in line broken mail check with dedicated function
Signed-off-by: SeongJae Park <sj38.park@gmail.com>

_hkml.py
@@ -103,6 +103,9 @@ class Mail:
         self.set_tags_series()
         hkml_cache.set_mail(self)
 
+    def broken(self):
+        return self.date is None or self.subject is None
+
     def to_kvpairs(self):
         if self.mbox == None:
             # ensure mbox is set.  TODO: don't parse it unnecessarily
@@ -189,7 +192,7 @@ def read_mbox_file(filepath):
     mails = []
     for message in mailbox.mbox(filepath):
         mail = Mail(mbox='%s' % message)
-        if mail.subject is None:
+        if not mail.broken():
             continue
         mails.append(mail)
     return mails

commit 8dadbd5e4a5a823843ad5a23f7aa8b1e45d336a8

_hkml/read_mbox_file(): Fix wrong broken mail filtering
Commit ebdb6742def6 ("_hkml: Replace in line broken mail check with
dedicated function") is wrongly filtering non-broken mails from reading.
Fix it.

Fixes: Commit ebdb6742def6 ("_hkml: Replace in line broken mail check with dedicated function")
Signed-off-by: SeongJae Park <sj38.park@gmail.com>

_hkml.py
@@ -197,7 +197,7 @@ def read_mbox_file(filepath):
 
     for message in mailbox.mbox(filepath):
         mail = Mail(mbox='%s' % message)
-        if not mail.broken():
+        if mail.broken():
             continue
         mails.append(mail)
     return mails