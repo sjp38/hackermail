227025ac587bf0d010d17e343a3c563861d7fc22
SeongJae Park <sj@kernel.org>
2024-01-25 22:22:31 -0800

    hkml_thread: Fix for updated mails_to_str()
    Signed-off-by: SeongJae Park <sj@kernel.org>

--- hkml_thread.py
+++ hkml_thread.py
@@ -26,7 +26,8 @@ def main(args=None):
     mails_to_show = hkml_list.last_listed_mails()
     to_show = hkml_list.mails_to_str(
             mails_to_show, None, None, None, None, False, args.mail_idx, False,
-            ['first_date'], None, None, None, args.lore, nr_cols_in_line)
+            ['first_date'], None, None, None, None, args.lore, nr_cols_in_line,
+            {}, False)
     hkml_open.pr_with_pager_if_needed(to_show)
 
 
