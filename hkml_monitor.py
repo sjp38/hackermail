87285bc7edaef4d07a5f442aa0f3975fe5abdbf4
SeongJae Park <sj38.park@gmail.com>
2024-01-27 12:26:29 -0800

    hkml_monitor: Add more options for add action
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- hkml_monitor.py
+++ hkml_monitor.py
@@ -19,10 +19,22 @@ def set_argparser(parser):
             title='action', dest='action', metavar='<action>')
 
     parser_add = subparsers.add_parser('add', help='add a monitoring request')
-    parser_add.add_argument('foo')
+    parser_add.add_argument(
+            'mailing_list', nargs='+', metavar='<mailing list>')
+    parser_add.add_argument('--sender', nargs='+', metavar='<keyword>')
+    parser_add.add_argument('--subject', nargs='+', metavar='<keyword>')
+    parser_add.add_argument('--body', nargs='+', metavar='<keyword>')
+    parser_add.add_argument('--thread_of', metavar='<mail id>')
+    parser_add.add_argument('--noti_mail', nargs='+', metavar='<email address>')
+    parser_add.add_argument('--noti_file', nargs='+', metavar='<file>')
+    parser_add.add_argument(
+            '--noti_period', metavar='<seconds>',
+            help='send notification once per this time')
+    parser_add.add_argument('--monitor_interval', metavar='<seconds>')
 
     parser_remove = subparsers.add_parser(
             'remove', help='remove a given monitoring request')
+    parser_remove.add_argument('req_index', type=int, metavar='<int>')
 
     parser_status = subparsers.add_parser(
             'status', help='show monitoring status including requests')
