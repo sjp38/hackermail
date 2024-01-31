56c4f17657dcaff735098800338761449d491e02
SeongJae Park <sj38.park@gmail.com>
2024-01-30 17:32:14 -0800

    hkml_thread: Fix mails_to_str() call
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- hkml_thread.py
+++ hkml_thread.py
@@ -59,9 +59,9 @@ def main(args=None):
 
     nr_cols_in_line = int(os.get_terminal_size().columns * 9 / 10)
     to_show = hkml_list.mails_to_str(
-            mails_to_show, None, None, None, None, False, args.mail_idx, False,
-            ['first_date'], None, None, None, None, args.lore, nr_cols_in_line,
-            [], False)
+            mails_to_show, None, None, None, None, None, None, False,
+            args.mail_idx, False, ['first_date'], None, None, None, None,
+            args.lore, nr_cols_in_line, [], False)
 
     if use_b4:
         hkml_cache.writeback_mails()
