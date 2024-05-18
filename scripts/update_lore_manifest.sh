bfa71a284deec86444e84defdd73adea88b8babb
SeongJae Park <sj38.park@gmail.com>
2024-05-18 11:44:58 -0700

    update_lore_manifest: Move to scripts/ directory
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- scripts/update_lore_manifest.sh
+++ scripts/update_lore_manifest.sh
@@ -4,6 +4,6 @@ bindir=$(dirname "$0")
 
 wget https://lore.kernel.org/manifest.js.gz
 gzip -d manifest.js.gz
-"$bindir/hkml" manifest convert_public_inbox_manifest \
+"$bindir/../hkml" manifest convert_public_inbox_manifest \
 	--public_inbox_manifest ./manifest.js \
-	--site https://lore.kernel.org > "$bindir/manifests/lore.js"
+	--site https://lore.kernel.org > "$bindir/../manifests/lore.js"
