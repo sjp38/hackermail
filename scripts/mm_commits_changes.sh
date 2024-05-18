2d63d63f8658a8217c8bc50a095da9b9f536b787
SeongJae Park <sj38.park@gmail.com>
2024-05-18 11:44:03 -0700

    Move mm_commits_changes.sh into scripts/ directory
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- scripts/mm_commits_changes.sh
+++ scripts/mm_commits_changes.sh
@@ -37,5 +37,5 @@ since=$(date --date="-$duration_days day" +%Y-%m-%d)
 
 bindir=$(dirname "$0")
 
-"$bindir/hkml" list mm-commits --fetch -cn --since "$since" --min_nr_mails 0 \
-	--stdout | "$bindir/__summary_mm_commits.py"
+"$bindir/../hkml" list mm-commits --fetch -cn --since "$since" \
+	--min_nr_mails 0 --stdout | "$bindir/__summary_mm_commits.py"
