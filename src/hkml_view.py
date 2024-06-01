c0bed820b4aa4da12d144540b90ed8bd6e33d2da
SeongJae Park <sj38.park@gmail.com>
2024-06-01 09:10:40 -0700

    src/hkml_view: Implement row focusing
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- src/hkml_view.py
+++ src/hkml_view.py
@@ -26,11 +26,21 @@ W: write new
 text_to_show = None
 
 def __view(stdscr):
+    focus_row = 0
+    curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
+    focus_color = curses.color_pair(1)
+    curses.init_pair(2, curses.COLOR_WHITE, curses.COLOR_BLACK)
+    normal_color = curses.color_pair(2)
+
     stdscr.clear()
     while True:
         rows, cols = stdscr.getmaxyx()
         for idx, line in enumerate(text_to_show.split('\n')):
-            stdscr.addstr(idx, 0, line)
+            if idx == focus_row:
+                color = focus_color
+            else:
+                color = normal_color
+            stdscr.addstr(idx, 0, line, color)
             if idx == rows - 1:
                 break
         x = stdscr.getch()
