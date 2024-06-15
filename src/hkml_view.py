sjp38/hackermail

commit d57a3e2d3e8581a2097a113aeae63ebcd07e7ba1

hkml_view: Writeback mail_idx_key_map when screen returns
From interactive list, if user opens a thread and come back, the
mail_idx_key_map is not updated back, so mail index based command works
inconsistently.  Fix it by writing mail_idx_key_map whenever drawing the
list, and the list has the map.

Signed-off-by: SeongJae Park <sj38.park@gmail.com>

src/hkml_view.py
@@ -56,6 +56,7 @@ class ScrollableList:
     focus_color = None
     normal_color = None
     input_handlers = None
+    mail_idx_key_map = None
 
     def __init__(self, screen, lines, focus_color, normal_color,
                  input_handlers):
@@ -111,6 +112,10 @@ class ScrollableList:
                     break
             if break_loop:
                 break
+            if self.mail_idx_key_map:
+                hkml_list.cache_list_str(
+                        'thread_output', '\n'.join(self.lines),
+                        self.mail_idx_key_map)
         return last_drawn
 
     def toast(self, message):

commit ffeb5c38c47474355d5e977086de400092495fcb

hkml_view: Writemap mail idx to key only if needed
Commit d57a3e2d3e85 ("hkml_view: Writeback mail_idx_key_map when screen
returns") does writeback of the map for every user input.  As a result,
scrolling became too slow.  Check if current map is the latest one and
do write back only if it is not.

Fixes: d57a3e2d3e85 ("hkml_view: Writeback mail_idx_key_map when screen returns")
Signed-off-by: SeongJae Park <sj38.park@gmail.com>

src/hkml_view.py
@@ -113,9 +113,11 @@ class ScrollableList:
             if break_loop:
                 break
             if self.mail_idx_key_map:
-                hkml_list.cache_list_str(
-                        'thread_output', '\n'.join(self.lines),
-                        self.mail_idx_key_map)
+                _, last_mail_idx_key_map = hkml_list.get_last_list()
+                if self.mail_idx_key_map != last_mail_idx_key_map:
+                    hkml_list.cache_list_str(
+                            'thread_output', '\n'.join(self.lines),
+                            self.mail_idx_key_map)
         return last_drawn
 
     def toast(self, message):