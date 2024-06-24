sjp38/hackermail

commit 1fc30895a2d44f3a4516e047a53d7b858cea6043

hkml_view/get_msgid_from_public_inbox_link(): Assume more aggressively
Public-inbox links could have unexpected URLs.  Simply assume every http
or https-based urls that having '@' as such URLs.

Signed-off-by: SeongJae Park <sj38.park@gmail.com>

src/hkml_view.py
@@ -323,20 +323,20 @@ def is_git_hash(word):
     return True
 
 def get_msgid_from_public_inbox_link(word):
-    site_urls = [_hkml.get_manifest()['site']]
-    site_urls += ['https://lkml.kernel.org', 'https://patch.msgid.link',
-                  'https://msgid.link']
-    not_public_inbox_link = True
-    for site_url in site_urls:
-        if word.startswith(site_url) and len(word) >= len(site_url) + 1:
-            not_public_inbox_link = False
-            break
-    if not_public_inbox_link:
+    '''
+    If it is http url and has @ in last field, assume it is msgid link
+    '''
+    if not word.startswith('http'):
         return None
     tokens = word.split('/')
+    if len(tokens) < 4:
+        return None
     if tokens[-1] == '':
-        return tokens[-2]
-    return tokens[-1]
+        msgid = tokens[-2]
+    msgid = tokens[-1]
+    if not '@' in msgid:
+        return None
+    return msgid
 
 def find_actionable_items(slist):
     line = slist.lines[slist.focus_row]

commit ba8710c10e50c1b1de02f80478d23fb060b55beb

hkml_view: fixup public-inbox link parsing for urls without trailing '/'
The public-inbox http[s] link parsing logic is broken for URLs not
having trailing '/'.  Fix it.

Fixes: 1fc30895a2d ("hkml_view/get_msgid_from_public_inbox_link(): Assume more aggressively")
Signed-off-by: SeongJae Park <sj38.park@gmail.com>

src/hkml_view.py
@@ -317,7 +317,8 @@ def get_msgid_from_public_inbox_link(word):
         return None
     if tokens[-1] == '':
         msgid = tokens[-2]
-    msgid = tokens[-1]
+    else:
+        msgid = tokens[-1]
     if not '@' in msgid:
         return None
     return msgid