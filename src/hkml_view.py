eb6efc64e699c6dd7758f05f4813c36f579f4174
SeongJae Park <sj38.park@gmail.com>
2024-06-29 09:15:13 -0700

    hkml_view: Split text viewer code to hkml_view_text.py
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- src/hkml_view.py
+++ src/hkml_view.py
@@ -17,6 +17,7 @@ import hkml_patch
 import hkml_reply
 import hkml_tag
 import hkml_thread
+import hkml_view_text
 import hkml_write
 
 '''
@@ -336,148 +337,6 @@ save_parent_content_menu_item_handler = [
         '- save parent screen content as ...',
         handle_save_content_menu_selection]
 
-# text viewer
-
-def text_viewer_menu_exec_git(c, slist):
-    words = slist.lines[slist.focus_row].split()[1:]
-    try:
-        output = subprocess.check_output(
-                words, stderr=subprocess.DEVNULL).decode().split('\n')
-    except Exception as e:
-        output = ['failed: %s' % e, '',
-                  'wrong commit id, or you are not on the git repo?']
-    show_text_viewer(slist.screen, output)
-
-def get_thread_txt_mail_idx_key_map(msgid):
-    thread_txt, mail_idx_key_map = hkml_thread.thread_str(msgid,
-            False, False)
-    hkml_cache.writeback_mails()
-    hkml_list.cache_list_str('thread_output', thread_txt, mail_idx_key_map)
-    return thread_txt, mail_idx_key_map
-
-def text_viewer_menu_hkml_thread(c, slist):
-    msgid = '<%s>' % slist.lines[slist.focus_row].split()[1:][-1]
-    thread_txt, mail_idx_key_map = get_thread_txt_mail_idx_key_map(msgid)
-    show_mails_list(slist.screen, thread_txt.split('\n'), mail_idx_key_map)
-
-def text_viewer_menu_hkml_open(c, slist):
-    msgid = '<%s>' % slist.lines[slist.focus_row].split()[1:][-1]
-    thread_txt, mail_idx_key_map = get_thread_txt_mail_idx_key_map(msgid)
-    for idx, cache_key in mail_idx_key_map.items():
-        mail = hkml_cache.get_mail(key=cache_key)
-        if mail is None:
-            continue
-        if mail.get_field('message-id') == msgid:
-            _, cols = slist.screen.getmaxyx()
-            lines = hkml_open.mail_display_str(mail, cols).split('\n')
-            show_text_viewer(slist.screen, lines)
-            break
-
-def text_viewer_menu_open_file(c, slist):
-    file_path = slist.lines[slist.focus_row].split()[1:][-1]
-    with open(file_path, 'r') as f:
-        lines = f.read().split('\n')
-    show_text_viewer(slist.screen, lines)
-
-def text_viewer_menu_vim_file(c, slist):
-    file_path = slist.lines[slist.focus_row].split()[1:][-1]
-    shell_mode_start(slist)
-    subprocess.call(['vim', file_path])
-    shell_mode_end(slist)
-
-def is_git_hash(word):
-    if len(word) < 10:
-        return False
-    for c in word:
-        if c not in '0123456789abcdef':
-            return False
-    return True
-
-def add_menus_for_commit(item_handlers, line):
-    for separator in [',', '(', ')', '/', '[', ']', '"']:
-        line = line.replace(separator, ' ')
-    for word in line.split():
-        if is_git_hash(word):
-            item_handlers.append(
-                    ['- git show %s' % word, text_viewer_menu_exec_git])
-            item_handlers.append(
-                    ['- git log -n 5 %s' % word, text_viewer_menu_exec_git])
-            item_handlers.append(
-                    ['- git log --oneline -n 64 %s' % word,
-                     text_viewer_menu_exec_git])
-
-def get_msgid_from_public_inbox_link(word):
-    '''
-    If it is http url and has @ in a field, assume it is msgid link
-    '''
-    if not word.startswith('http'):
-        return None
-    tokens = word.split('/')
-    if len(tokens) < 4:
-        return None
-    for token in tokens[3:]:
-        if '@' in token:
-            return token
-    return None
-
-def add_menus_for_msgid(item_handlers, line):
-    for separator in [',', '(', ')', '[', ']', '"']:
-        line = line.replace(separator, ' ')
-    for word in line.split():
-        msgid = get_msgid_from_public_inbox_link(word)
-        if msgid is not None:
-            item_handlers.append(
-                    ['- hkml thread %s' % msgid, text_viewer_menu_hkml_thread])
-            item_handlers.append(
-                    ['- hkml open %s' % msgid, text_viewer_menu_hkml_open])
-
-def add_menus_for_files(item_handlers, line):
-    for separator in [',', '(', ')', '[', ']', '"']:
-        line = line.replace(separator, ' ')
-
-    found_files = {}
-    for word in line.split():
-        # file paths on diff starts with a/ and b/, e.g.,
-        #
-        # --- a/tools/testing/selftests/damon/damon_nr_regions.py
-        # +++ b/tools/testing/selftests/damon/damon_nr_regions.py
-        if word.startswith('a/') or word.startswith('b/'):
-            word = word[2:]
-        if not word in found_files and os.path.isfile(word):
-            found_files[word] = True
-            item_handlers.append(
-                    ['- hkml open file %s' % word, text_viewer_menu_open_file])
-            item_handlers.append(
-                    ['- vim %s' % word, text_viewer_menu_vim_file])
-
-def build_text_view_menu_item_handlers(slist):
-    line = slist.lines[slist.focus_row]
-
-    item_handlers = []
-    add_menus_for_commit(item_handlers, line)
-    add_menus_for_msgid(item_handlers, line)
-    add_menus_for_files(item_handlers, line)
-
-    item_handlers.append(save_parent_content_menu_item_handler)
-    return item_handlers
-
-def show_text_viewer_menu(c, slist):
-    item_handlers = build_text_view_menu_item_handlers(slist)
-    lines = ['selected line: %s' % slist.lines[slist.focus_row], '',
-             'focus an item below and press Enter', '']
-    menu_list = ScrollableList(slist.screen, lines, None)
-    menu_list.set_menu_item_handlers(slist, item_handlers)
-    menu_list.draw()
-
-def get_text_viewer_handlers():
-    return scrollable_list_default_handlers() + [
-            InputHandler(['m', '\n'], show_text_viewer_menu, 'open menu')
-                ]
-
-def show_text_viewer(screen, text_lines):
-    slist = ScrollableList(screen, text_lines, get_text_viewer_handlers())
-    slist.draw()
-    return slist
 
 # mails list
 
@@ -512,7 +371,7 @@ def open_focused_mail(c, slist):
 
     _, cols = slist.screen.getmaxyx()
     lines = hkml_open.mail_display_str(mail, cols).split('\n')
-    show_text_viewer(slist.screen, lines)
+    hkml_view_text.show_text_viewer(slist.screen, lines)
 
 def get_attach_files():
     answer = input('Do you want to attach files to the mail? [y/N] ')
@@ -789,7 +648,7 @@ def __view(stdscr, text_to_show, mail_idx_key_map):
 
     if mail_idx_key_map is not None:
         return show_mails_list(stdscr, text_lines, mail_idx_key_map)
-    return show_text_viewer(stdscr, text_lines)
+    return hkml_view_text.show_text_viewer(stdscr, text_lines)
 
 def view(text, mail_idx_key_map):
     try:
---
+++ src/hkml_view_text.py
@@ -0,0 +1,156 @@
+# SPDX-License-Identifier: GPL-2.0
+
+# text viewer
+
+import os
+import subprocess
+
+import hkml_cache
+import hkml_list
+import hkml_open
+import hkml_thread
+import hkml_view
+
+def text_viewer_menu_exec_git(c, slist):
+    words = slist.lines[slist.focus_row].split()[1:]
+    try:
+        output = subprocess.check_output(
+                words, stderr=subprocess.DEVNULL).decode().split('\n')
+    except Exception as e:
+        output = ['failed: %s' % e, '',
+                  'wrong commit id, or you are not on the git repo?']
+    show_text_viewer(slist.screen, output)
+
+def get_thread_txt_mail_idx_key_map(msgid):
+    thread_txt, mail_idx_key_map = hkml_thread.thread_str(msgid,
+            False, False)
+    hkml_cache.writeback_mails()
+    hkml_list.cache_list_str('thread_output', thread_txt, mail_idx_key_map)
+    return thread_txt, mail_idx_key_map
+
+def text_viewer_menu_hkml_thread(c, slist):
+    msgid = '<%s>' % slist.lines[slist.focus_row].split()[1:][-1]
+    thread_txt, mail_idx_key_map = get_thread_txt_mail_idx_key_map(msgid)
+    hkml_view.show_mails_list(slist.screen, thread_txt.split('\n'),
+                              mail_idx_key_map)
+
+def text_viewer_menu_hkml_open(c, slist):
+    msgid = '<%s>' % slist.lines[slist.focus_row].split()[1:][-1]
+    thread_txt, mail_idx_key_map = get_thread_txt_mail_idx_key_map(msgid)
+    for idx, cache_key in mail_idx_key_map.items():
+        mail = hkml_cache.get_mail(key=cache_key)
+        if mail is None:
+            continue
+        if mail.get_field('message-id') == msgid:
+            _, cols = slist.screen.getmaxyx()
+            lines = hkml_open.mail_display_str(mail, cols).split('\n')
+            show_text_viewer(slist.screen, lines)
+            break
+
+def text_viewer_menu_open_file(c, slist):
+    file_path = slist.lines[slist.focus_row].split()[1:][-1]
+    with open(file_path, 'r') as f:
+        lines = f.read().split('\n')
+    show_text_viewer(slist.screen, lines)
+
+def text_viewer_menu_vim_file(c, slist):
+    file_path = slist.lines[slist.focus_row].split()[1:][-1]
+    hkml_view.shell_mode_start(slist)
+    subprocess.call(['vim', file_path])
+    hkml_view.shell_mode_end(slist)
+
+def is_git_hash(word):
+    if len(word) < 10:
+        return False
+    for c in word:
+        if c not in '0123456789abcdef':
+            return False
+    return True
+
+def add_menus_for_commit(item_handlers, line):
+    for separator in [',', '(', ')', '/', '[', ']', '"']:
+        line = line.replace(separator, ' ')
+    for word in line.split():
+        if is_git_hash(word):
+            item_handlers.append(
+                    ['- git show %s' % word, text_viewer_menu_exec_git])
+            item_handlers.append(
+                    ['- git log -n 5 %s' % word, text_viewer_menu_exec_git])
+            item_handlers.append(
+                    ['- git log --oneline -n 64 %s' % word,
+                     text_viewer_menu_exec_git])
+
+def get_msgid_from_public_inbox_link(word):
+    '''
+    If it is http url and has @ in a field, assume it is msgid link
+    '''
+    if not word.startswith('http'):
+        return None
+    tokens = word.split('/')
+    if len(tokens) < 4:
+        return None
+    for token in tokens[3:]:
+        if '@' in token:
+            return token
+    return None
+
+def add_menus_for_msgid(item_handlers, line):
+    for separator in [',', '(', ')', '[', ']', '"']:
+        line = line.replace(separator, ' ')
+    for word in line.split():
+        msgid = get_msgid_from_public_inbox_link(word)
+        if msgid is not None:
+            item_handlers.append(
+                    ['- hkml thread %s' % msgid, text_viewer_menu_hkml_thread])
+            item_handlers.append(
+                    ['- hkml open %s' % msgid, text_viewer_menu_hkml_open])
+
+def add_menus_for_files(item_handlers, line):
+    for separator in [',', '(', ')', '[', ']', '"']:
+        line = line.replace(separator, ' ')
+
+    found_files = {}
+    for word in line.split():
+        # file paths on diff starts with a/ and b/, e.g.,
+        #
+        # --- a/tools/testing/selftests/damon/damon_nr_regions.py
+        # +++ b/tools/testing/selftests/damon/damon_nr_regions.py
+        if word.startswith('a/') or word.startswith('b/'):
+            word = word[2:]
+        if not word in found_files and os.path.isfile(word):
+            found_files[word] = True
+            item_handlers.append(
+                    ['- hkml open file %s' % word, text_viewer_menu_open_file])
+            item_handlers.append(
+                    ['- vim %s' % word, text_viewer_menu_vim_file])
+
+def build_text_view_menu_item_handlers(slist):
+    line = slist.lines[slist.focus_row]
+
+    item_handlers = []
+    add_menus_for_commit(item_handlers, line)
+    add_menus_for_msgid(item_handlers, line)
+    add_menus_for_files(item_handlers, line)
+
+    item_handlers.append(hkml_view.save_parent_content_menu_item_handler)
+    return item_handlers
+
+def show_text_viewer_menu(c, slist):
+    item_handlers = build_text_view_menu_item_handlers(slist)
+    lines = ['selected line: %s' % slist.lines[slist.focus_row], '',
+             'focus an item below and press Enter', '']
+    menu_list = hkml_view.ScrollableList(slist.screen, lines, None)
+    menu_list.set_menu_item_handlers(slist, item_handlers)
+    menu_list.draw()
+
+def get_text_viewer_handlers():
+    return hkml_view.scrollable_list_default_handlers() + [
+            hkml_view.InputHandler(
+                ['m', '\n'], show_text_viewer_menu, 'open menu')
+                ]
+
+def show_text_viewer(screen, text_lines):
+    slist = hkml_view.ScrollableList(
+            screen, text_lines, get_text_viewer_handlers())
+    slist.draw()
+    return slist
