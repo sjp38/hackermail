113858d8e10ac68ead9a689acbe59f586ef96541
SeongJae Park <sj38.park@gmail.com>
2024-07-14 13:57:20 -0700

    hkml: Add interactive mode
    Let 'hkml' without command to start interactive mode.  This commit only
    adds the command line handling, though.  The feature will be implemented
    by following commits.
    
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- src/hkml.py
+++ src/hkml.py
@@ -7,6 +7,7 @@ import subprocess
 import sys
 
 import hkml_init
+import hkml_interactive
 import hkml_fetch
 import hkml_list
 import hkml_thread
@@ -41,6 +42,7 @@ parser.add_argument('-C', '--directory', metavar='<dir>',
 
 subparsers = parser.add_subparsers(title='command', dest='command',
         metavar='<command>')
+subparsers.default = 'interactive'
 
 parser_init = subparsers.add_parser('init', help = 'initialize working dir')
 hkml_init.set_argparser(parser_init)
@@ -139,6 +141,8 @@ elif args.command == 'manifest':
     hkml_manifest.main(args)
 elif args.command == 'cache':
     hkml_cache.main(args)
+elif args.command == 'interactive':
+    hkml_interactive.main(args)
 else:
     print('wrong command (%s)' % args.command)
     exit(1)
