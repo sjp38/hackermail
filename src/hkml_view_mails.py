sjp38/hackermail

commit aa4ed4a952a009e258d353816e915df2fc7fd167

hkml_view_mails: Split out the logic to forwarding the given mail
Signed-off-by: SeongJae Park <sj38.park@gmail.com>

src/hkml_view_mails.py
@@ -82,14 +82,17 @@ def reply_focused_mail(c, slist):
 
     reply_mail(slist, mail)
 
-def forward_focused_mail(c, slist):
-    mail = get_focused_mail(slist)
-    if mail is None:
-        return
+def forward_mail(slist, mail):
     hkml_view.shell_mode_start(slist)
     files = get_attach_files()
     hkml_forward.forward(mail, attach_files=files)
     hkml_view.shell_mode_end(slist)
+
+def forward_focused_mail(c, slist):
+    mail = get_focused_mail(slist)
+    if mail is None:
+        return
+    forward_mail(mail, slist)
 
 def list_thread_of_focused_mail(c, slist):
     thread_txt, mail_idx_key_map = hkml_thread.thread_str(

commit 3f45f63c6b0f0b3202fb451b5ef04afe32bb6d71

hkml_view_mails: Fix wrong forward_mail() call
forward_focused_mail() is wrongly calling forward_mail().  Fix it.

Fixes: aa4ed4a ("hkml_view_mails: Split out the logic to forwarding the given mail")
Signed-off-by: SeongJae Park <sj38.park@gmail.com>

src/hkml_view_mails.py
@@ -92,7 +92,7 @@ def forward_focused_mail(c, slist):
     mail = get_focused_mail(slist)
     if mail is None:
         return
-    forward_mail(mail, slist)
+    forward_mail(slist, mail)
 
 def list_thread_of_focused_mail(c, slist):
     thread_txt, mail_idx_key_map = hkml_thread.thread_str(