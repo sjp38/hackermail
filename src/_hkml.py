75b9526f3b07a823b241f725daa1e5db0933561a
SeongJae Park <sj@kernel.org>
2025-03-29 11:31:27 -0700

    _hkml: set in-reply-to-msgid for atom-parsed mail always
    Since in-reply-to-msgid is not set when in-reply-to field is not found,
    mail listing always asks atom-parsed mails to have proper mbox field,
    which requires public inbox query.  Set in-reply-to-msgid always, to
    avoid unnecessary public inbox traffic.
    
    Signed-off-by: SeongJae Park <sj@kernel.org>

--- src/_hkml.py
+++ src/_hkml.py
@@ -133,6 +133,7 @@ class Mail:
                 self.__fields['in-reply-to'] = link[http_prefix_len:-1]
         if not 'in-reply-to' in self.__fields:
             self.__fields['in-reply-to'] = None
+            self.__fields['in-reply-to-msgid'] = None
         else:
             # some mail puts infomration in addition to message id on in-reply-to
             # header.  E.g., 87ikvefswp.fsf@yhuang6-desk2.ccr.corp.intel.com
