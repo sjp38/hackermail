50b35d1e03ac80b4e1c7b1b578bc853149ecca5c
SeongJae Park <sj@kernel.org>
2025-05-03 10:07:36 -0700

    Add a helper module for CLI
    hkml_view.py has CLI helper code that can also be used from general
    code.  Add a helper module that having a copy of the helper code.  It
    will eventually replace that on hkml_view.py.
    
    Signed-off-by: SeongJae Park <sj@kernel.org>

---
+++ src/_hkml_cli.py
@@ -0,0 +1,102 @@
+# SPDX-License-Identifier: GPL-2.0
+
+'''
+object for CliQuestion.ask_selection()
+'text' is displayed to the user with the selection question.
+'handle_fn' of user-selected one is called if it is not None.  The function
+    receives user-set CliQuestion.data, user-entered input (number), and the
+    selected CliSelection object.
+'data' can save any selection-specific data.
+'''
+class CliSelection:
+    text = None
+    handle_fn = None    # function receiving question data, answer, selection
+    data = None         # for carrying selection-specific data
+
+    def __init__(self, text, handle_fn=None, data=None):
+        self.text = text
+        self.handle_fn = handle_fn
+        self.data = data
+
+'''
+question that will be provided to the user, in shell mode.
+ask_input() and ask_selection() are the methods that user will really use.
+'''
+class CliQuestion:
+    description = None
+    prompt = None
+
+    def __init__(self, prompt=None, desc=None):
+        self.description = desc
+        self.prompt = prompt
+
+    '''
+    internal method.  Shouldn't be called directly from CliQuestion user.
+    '''
+    def ask(self, data, selections, handle_fn, notify_completion):
+        # return answer, selection, and error
+        lines = []
+        if self.description is not None:
+            lines.append(self.description)
+            lines.append('')
+        if selections is not None:
+            for idx, selection in enumerate(selections):
+                lines.append('%d: %s' % (idx + 1, selection.text))
+            lines.append('')
+        if len(lines) > 0:
+            print('\n'.join(lines))
+
+        answer = input('%s (enter \'\' to cancel): ' % self.prompt)
+        if answer == '':
+            print('Canceled.')
+            return None, None, 'canceled'
+
+        selection = None
+        selection_handle_fn = None
+        if selections is not None:
+            try:
+                selection = selections[int(answer) - 1]
+                selection_handle_fn = selection.handle_fn
+            except:
+                print('Wrong input.')
+                return None, None, 'wrong input'
+
+        if selection_handle_fn is not None:
+            err = selection_handle_fn(data, answer, selection)
+        elif handle_fn is not None:
+            err = handle_fn(data, answer)
+            if err:
+                return None, None, err
+
+        if notify_completion:
+            print('Done.')
+        return answer, selection, None
+
+    '''
+    Ask user to answer any text input.  If handle_fn is not None, the function
+    is called with 'data' and user's input to the question.
+
+    Should be invoked in shell mode (show shell_mode_start() and
+    shell_mode_end()).
+
+    Returns user's input to the question and error.
+    '''
+    def ask_input(self, data=None, handle_fn=None, notify_completion=False):
+        answer, selection, err = self.ask(data, None, handle_fn,
+                                          notify_completion)
+        return answer, err
+
+    '''
+    Ask user to select on of CliSelection objects.  If the selected
+    CliSelection has handle_fn field set, it is invoked.
+
+    Should be invoked in shell mode (show shell_mode_start() and
+    shell_mode_end()).
+
+    Returns user's input to the question, selected CliSelection, and error.
+    '''
+    def ask_selection(self, selections, data=None, notify_completion=False):
+        if self.prompt is None:
+            self.prompt = 'Enter the item number'
+        return self.ask(data, selections, None, notify_completion)
+
