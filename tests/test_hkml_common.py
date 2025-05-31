157b350134a31924dbf8bcefd271850d9e0b2e34
SeongJae Park <sj@kernel.org>
2025-05-31 10:23:13 -0700

    tests: add test for hkml_common.parse_date_diff()
    Signed-off-by: SeongJae Park <sj@kernel.org>

---
+++ tests/test_hkml_common.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# SPDX-License-Identifier: GPL-2.0
+
+import datetime
+import unittest
+import os
+import sys
+
+bindir = os.path.dirname(os.path.realpath(__file__))
+src_dir = os.path.join(bindir, '..', 'src')
+sys.path.append(src_dir)
+
+import hkml_common
+
+class TestHkmlCommon(unittest.TestCase):
+    def test_parse_date_diff_input(self):
+        now = datetime.datetime(2025, 5, 31, 10, 18)
+        self.assertEqual(
+                hkml_common.parse_date_diff('-2 days', now),
+                datetime.datetime(2025, 5, 29, 10, 18))
+        self.assertEqual(
+                hkml_common.parse_date_diff('+1 days', now),
+                datetime.datetime(2025, 6, 1, 10, 18))
+
+if __name__ == '__main__':
+    unittest.main()
