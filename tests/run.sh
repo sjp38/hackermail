b3c8ff0449235c8f54e4a3d3287eac3957b63839
SeongJae Park <sj@kernel.org>
2025-06-15 13:45:21 -0700

    tests/run: use tab for indentation
    Tabs and spaces are mixed in run.sh file.  Use tabs only.
    
    Signed-off-by: SeongJae Park <sj@kernel.org>

--- tests/run.sh
+++ tests/run.sh
@@ -9,26 +9,26 @@ if [ "$1" == "coverage" ]; then
 
 	if ! python3 -m coverage help &> /dev/null
 	then
-                echo "coverage not found, install it (e.g., 'pip3 install coverage')" >&2
-                exit 1
-        fi
+		echo "coverage not found, install it (e.g., 'pip3 install coverage')" >&2
+		exit 1
+	fi
 
-        python3 -m coverage erase
-        python_binary="python3 -m coverage run -a"
+	python3 -m coverage erase
+	python_binary="python3 -m coverage run -a"
 fi
 
 for test_file in "$bindir"/test_*.py
 do
-        if $python_binary "$test_file" &> /dev/null
-        then
-                echo "PASS unit $(basename $test_file)"
-        else
-                echo "FAIL unit $(basename $test_file)"
-                exit 1
-        fi
+	if $python_binary "$test_file" &> /dev/null
+	then
+		echo "PASS unit $(basename $test_file)"
+	else
+		echo "FAIL unit $(basename $test_file)"
+		exit 1
+	fi
 done
 
 if [ "$1" == "coverage" ]; then
-        python3 -m coverage xml
-        python3 -m coverage report
+	python3 -m coverage xml
+	python3 -m coverage report
 fi
