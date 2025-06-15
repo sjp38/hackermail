73133cb587efc178aaf9dc1dbd2d4d613e7df256
Mykola Lysenko <mykolal@meta.com>
2025-06-14 21:15:20 -0700

    Add coverage infrastructure
    Signed-off-by: Mykola Lysenko <mykolal@meta.com>

--- tests/run.sh
+++ tests/run.sh
@@ -1,15 +1,33 @@
 #!/bin/bash
 # SPDX-License-Identifier: GPL-2.0
 
+
 bindir=$(dirname "$0")
+python_binary="python3"
+
+if [ "$1" == "coverage" ]; then
+
+        if [ -z "$(which coverage 2>/dev/null)" ]; then
+                echo "coverage not found, install it with 'pip3 install coverage'" >&2
+                exit 1
+        fi
+
+        coverage erase
+        python_binary="coverage run -a"
+fi
 
 for test_file in "$bindir"/test_*.py
 do
-	if python3 "$test_file" &> /dev/null
-	then
-		echo "PASS unit $(basename $test_file)"
-	else
-		echo "FAIL unit $(basename $test_file)"
-		exit 1
-	fi
+        if $python_binary "$test_file" &> /dev/null
+        then
+                echo "PASS unit $(basename $test_file)"
+        else
+                echo "FAIL unit $(basename $test_file)"
+                exit 1
+        fi
 done
+
+if [ "$1" == "coverage" ]; then
+        coverage xml
+        coverage report
+fi
