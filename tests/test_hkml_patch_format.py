80b7e5f97edb2a3cb18dae84df854dd18cbbe819
SeongJae Park <sj@kernel.org>
2025-08-23 10:19:18 -0700

    test_hkml_patch_format: do not check invalid reason
    Signed-off-by: SeongJae Park <sj@kernel.org>

--- tests/test_hkml_patch_format.py
+++ tests/test_hkml_patch_format.py
@@ -12,17 +12,16 @@ sys.path.append(src_dir)
 import hkml_patch_format
 
 class TestHkmlPatchFormat(unittest.TestCase):
-    def assert_is_valid_subject_prefix(self, prefix, is_valid, invalid_reason):
+    def assert_is_valid_subject_prefix(self, prefix, is_valid):
         is_valid_, invalid_reason_ = hkml_patch_format.is_valid_subject_prefix(
                 prefix)
         self.assertEqual(is_valid, is_valid_)
-        self.assertEqual(invalid_reason, invalid_reason_)
 
     def test_is_valid_subject_prefix(self):
-        self.assert_is_valid_subject_prefix('RFC PATCH', True, None)
-        self.assert_is_valid_subject_prefix('PATCH v2', True, None)
-        self.assert_is_valid_subject_prefix('PATCH v2 mm-unstable', True, None)
-        self.assert_is_valid_subject_prefix('PATCH 6.12.y', True, None)
+        self.assert_is_valid_subject_prefix('RFC PATCH', True)
+        self.assert_is_valid_subject_prefix('PATCH v2', True)
+        self.assert_is_valid_subject_prefix('PATCH v2 mm-unstable', True)
+        self.assert_is_valid_subject_prefix('PATCH 6.12.y', True)
 
 if __name__ == '__main__':
     unittest.main()
