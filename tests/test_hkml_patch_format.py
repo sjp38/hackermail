0a82839ebf46342502275f2d05009ddfa0107c9c
SeongJae Park <sj@kernel.org>
2025-08-31 19:39:01 -0700

    test_hkml_patch_format: test parse_subject_prefix()
    Signed-off-by: SeongJae Park <sj@kernel.org>

--- tests/test_hkml_patch_format.py
+++ tests/test_hkml_patch_format.py
@@ -35,5 +35,40 @@ class TestHkmlPatchFormat(unittest.TestCase):
         self.assert_is_valid_subject_prefix('PATCH 6.12.y foo', False)
         self.assert_is_valid_subject_prefix('PATCH v2 v4', False)
 
+    def assert_parse_subject_prefix(self, prefix, expect_success, expects):
+        invalid_reason, is_rfc, version_nr, sequence, target_tree = \
+                hkml_patch_format.parse_subject_prefix(prefix)
+        if not expect_success:
+            self.assertTrue(invalid_reason != None)
+            return
+        self.assertTrue(invalid_reason == None)
+        self.assertEqual([is_rfc, version_nr, sequence, target_tree], expects)
+
+    def test_parse_subject_prefix(self):
+        self.assert_parse_subject_prefix('RFC PATCH', True,
+                                         [True, None, None, None])
+        self.assert_parse_subject_prefix('PATCH v2', True,
+                                         [False, 'v2', None, None])
+        self.assert_parse_subject_prefix('PATCH v2 00/12', True,
+                                         [False, 'v2', '00/12', None])
+        self.assert_parse_subject_prefix('PATCH v2 03/12', True,
+                                         [False, 'v2', '03/12', None])
+        self.assert_parse_subject_prefix('PATCH v2 mm-unstable', True,
+                                         [False, 'v2', None, 'mm-unstable'])
+        self.assert_parse_subject_prefix('PATCH v2 mm-unstable 03/12', True,
+                                         [False, 'v2', '03/12', 'mm-unstable'])
+        self.assert_parse_subject_prefix('PATCH 6.12.y', True,
+                                         [False, None, None, '6.12.y'])
+        self.assert_parse_subject_prefix('PATCH 6.12.y v3', True,
+                                         [False, 'v3', None, '6.12.y'])
+
+        self.assert_parse_subject_prefix('foo', False, None)
+        self.assert_parse_subject_prefix('RFC foo', False, None)
+        self.assert_parse_subject_prefix('foo v2', False, None)
+        # multiple sequence
+        self.assert_parse_subject_prefix('PATCH 03/12 04/12', False, None)
+        self.assert_parse_subject_prefix('PATCH 6.12.y foo', False, None)
+        self.assert_parse_subject_prefix('PATCH v2 v4', False, None)
+
 if __name__ == '__main__':
     unittest.main()
