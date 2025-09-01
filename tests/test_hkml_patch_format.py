b6b6be3c221ead2d7b5d624cbfde22847c1438f3
SeongJae Park <sj@kernel.org>
2025-08-31 19:42:00 -0700

    test_hkml_patch_format: stop testing is_valid_subject_prefix()
    It is alternated by parse_subject_prefix().  Stop testing unused
    function.
    
    Signed-off-by: SeongJae Park <sj@kernel.org>

--- tests/test_hkml_patch_format.py
+++ tests/test_hkml_patch_format.py
@@ -12,29 +12,6 @@ sys.path.append(src_dir)
 import hkml_patch_format
 
 class TestHkmlPatchFormat(unittest.TestCase):
-    def assert_is_valid_subject_prefix(self, prefix, is_valid):
-        is_valid_, invalid_reason_ = hkml_patch_format.is_valid_subject_prefix(
-                prefix)
-        self.assertEqual(is_valid, is_valid_)
-
-    def test_is_valid_subject_prefix(self):
-        self.assert_is_valid_subject_prefix('RFC PATCH', True)
-        self.assert_is_valid_subject_prefix('PATCH v2', True)
-        self.assert_is_valid_subject_prefix('PATCH v2 00/12', True)
-        self.assert_is_valid_subject_prefix('PATCH v2 03/12', True)
-        self.assert_is_valid_subject_prefix('PATCH v2 mm-unstable', True)
-        self.assert_is_valid_subject_prefix('PATCH v2 mm-unstable 03/12', True)
-        self.assert_is_valid_subject_prefix('PATCH 6.12.y', True)
-        self.assert_is_valid_subject_prefix('PATCH 6.12.y v3', True)
-
-        self.assert_is_valid_subject_prefix('foo', False)
-        self.assert_is_valid_subject_prefix('RFC foo', False)
-        self.assert_is_valid_subject_prefix('foo v2', False)
-        # multiple sequence
-        self.assert_is_valid_subject_prefix('PATCH 03/12 04/12', False)
-        self.assert_is_valid_subject_prefix('PATCH 6.12.y foo', False)
-        self.assert_is_valid_subject_prefix('PATCH v2 v4', False)
-
     def assert_parse_subject_prefix(self, prefix, expect_success, expects):
         invalid_reason, is_rfc, version_nr, sequence, target_tree = \
                 hkml_patch_format.parse_subject_prefix(prefix)
