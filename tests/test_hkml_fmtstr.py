a0c0d523c18838d9bd246f12f66f6e8f35f50dc0
SeongJae Park <sj@kernel.org>
2025-11-15 12:52:39 -0800

    tests: add a test for wrap_line()
    Signed-off-by: SeongJae Park <sj@kernel.org>

---
+++ tests/test_hkml_fmtstr.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# SPDX-License-Identifier: GPL-2.0
+
+import unittest
+import os
+import sys
+
+bindir = os.path.dirname(os.path.realpath(__file__))
+src_dir = os.path.join(bindir, '..', 'src')
+sys.path.append(src_dir)
+
+import _hkml_fmtstr
+
+class TestHkmlFmtstr(unittest.TestCase):
+    def test_wrap_line(self):
+        self.assertEqual(
+                _hkml_fmtstr.wrap_line('[something]', 'foo bar baz asdf', 20),
+                ['[something] foo bar',
+                 '            baz asdf',
+                 ])
+
+if __name__ == '__main__':
+    unittest.main()
