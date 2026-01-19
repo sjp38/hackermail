4ceee3aba569d0c038b74255752cf29672fb4de6
SeongJae Park <sj@kernel.org>
2026-01-19 14:21:27 -0800

    hkml_write: set reply coloring by default
    Now users are well notified about why the coloring is applied, and how
    it can be disabled.  Do it by default.
    
    Signed-off-by: SeongJae Park <sj@kernel.org>

--- src/hkml_write.py
+++ src/hkml_write.py
@@ -196,6 +196,7 @@ def open_editor(file_path, target_desc='mail', cursor_row=0, is_reply=False):
                     ]),
                 selections_txt=['Yes, do the past lines coloring.',
                                 'No, I\'m good.'],
+                default_selection_idx=0,
                 allow_cancel=False, allow_error=False)
         if selection == 0:
             options = []
