a041a3fdf77d607469461a1404a3fe59acc7ebd6
SeongJae Park <sj@kernel.org>
2026-03-14 10:08:25 -0700

    hkml_view_mails: show sashiko.dev link on mail menu
    sashiko.dev provides AI review results of patches.  It is useful.
    Provide a link to that for given mail.
    
    Signed-off-by: SeongJae Park <sj@kernel.org>

--- src/hkml_view_mails.py
+++ src/hkml_view_mails.py
@@ -1013,6 +1013,10 @@ def show_mails_list_menu(c, slist):
     else:
         menu_desc = 'selected mail: %s' % mail.subject
 
+    msgid = mail.get_field('message-id')[1:-1]
+    menu_desc += '\n\nsashiko.dev link: https://sashiko.dev/#/patchset/%s' % \
+            msgid
+
     hkml_view.shell_mode_start(slist)
     _hkml_cli.ask_selection(
             desc=menu_desc,
