7c445277b842216f3e3b5a4cf043c16548aca17b
SeongJae Park <sj@kernel.org>
2026-03-20 19:06:29 -0700

    _hkml_sashiko_dev: implement a function to fetch all reviews of a thread
    Signed-off-by: SeongJae Park <sj@kernel.org>

--- src/_hkml_sashiko_dev.py
+++ src/_hkml_sashiko_dev.py
@@ -74,3 +74,37 @@ def get_review(msgid):
     if msgid in msgid_output_cache:
         return msgid_output_cache[msgid], None
     return None, 'no review found'
+
+def get_reviews(msgid):
+    '''
+    Return SashikoDevReview objects for the thread and an error
+    '''
+    global session
+    if session is None:
+        session = requests.session()
+        session.headers.update({'User-Agent': 'hkml/1.5.4'})
+    resp = session.get('https://sashiko.dev/api/patch',
+                       params={'id': msgid}, timeout=10)
+    if resp.status_code != 200:
+        return None, 'get response is not 200 but %s' % resp.status_code
+    data = resp.json()
+
+    id_patch_map = {}
+    for patch in data['patches']:
+        id_patch_map[patch['id']] = patch
+
+    sashiko_reviews = []
+    for idx, review in enumerate(data['reviews']):
+        result = review['result']
+        status = review['status']
+        patch_id = review['patch_id']
+        patch = id_patch_map[patch_id]
+        patch_msgid = patch['message_id']
+        patch_subject = patch['subject']
+        inline_review = review['inline_review']
+
+        sashiko_dev_review = SashikoDevReview(
+                result, status, patch_msgid, patch_subject, inline_review)
+        msgid_output_cache[patch_msgid] = sashiko_dev_review
+        sashiko_reviews.append(sashiko_dev_review)
+    return sashiko_reviews, None
