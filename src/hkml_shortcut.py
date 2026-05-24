61ba46d0edb354b87b08d225fcd4c9387df2b2b8
SeongJae Park <sj@kernel.org>
2026-05-24 13:28:06 -0700

    hkml_shortcut: add user configs file read/write functions
    Signed-off-by: SeongJae Park <sj@kernel.org>

--- src/hkml_shortcut.py
+++ src/hkml_shortcut.py
@@ -20,6 +20,54 @@ $ hkml shortcut remove list f
 '''
 
 import argparse
+import json
+import os
+
+import _hkml
+
+class UserConfig:
+    category = None
+    key_input = None
+    action = None
+    arguments = None
+
+    def __init__(self, category, key_input, action, arguments):
+        self.category = category
+        self.key_input = key_input
+        self.action = action
+        self.arguments = arguments
+
+    def to_kvpairs(self):
+        return {
+                'category': self.category,
+                'key_input': self.key_input,
+                'action': self.action,
+                'arguments': self.arguments,
+                }
+
+    @classmethod
+    def from_kvpairs(cls, kvpairs):
+        return cls(category=kvpairs['category'],
+                   key_input=kvpairs['key_input'], action=kvpairs['action'],
+                   arguments=kvpairs['arguments'])
+
+def user_configs_file_path():
+    return os.path.join(_hkml.get_hkml_dir(), 'shortcut_configs')
+
+def write_user_configs_file(user_configs):
+    kvpairs = {
+            'configs': [config.to_kvpairs() for config in user_configs],
+            }
+    with open(user_configs_file_path(), 'w') as f:
+        json.dump(kvpairs, f, indent=4, sort_keys=True)
+
+def read_user_configs_file():
+    file_path = user_configs_file_path()
+    if not os.path.isfile(file_path):
+        return []
+    with open(file_path, 'r') as f:
+        kvpairs = json.load(f)
+    return [UserConfig.from_kvpairs(kvp) for kvp in kvpairs['configs']]
 
 def main(args):
     print('not yet implemented')
