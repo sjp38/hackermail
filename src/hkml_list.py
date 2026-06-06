caa8394e63351eb41cad53f53dfde1bfc57dd807
SeongJae Park <sj@kernel.org>
2026-06-06 09:40:10 -0700

    hkml_list: add MailsListData.mail_items
    Signed-off-by: SeongJae Park <sj@kernel.org>

--- src/hkml_list.py
+++ src/hkml_list.py
@@ -736,14 +736,16 @@ class MailsListData:
     mail_idx_key_map = None
     # For list output cache
     mails_cache_data = None
+    mail_items = None    # list of MailListMailItem
 
     def __init__(self, text, len_comments, line_nr_mail_map, mail_idx_key_map,
-                 mails_cache_data=None):
+                 mails_cache_data=None, mail_items=None):
         self.text = text
         self.len_comments = len_comments
         self.line_nr_mail_map = line_nr_mail_map
         self.mail_idx_key_map = mail_idx_key_map
         self.mails_cache_data = mails_cache_data
+        self.mail_items = mail_items
 
         lines = text.split('\n')
         # in case of cached list, len_comments is passed as None
