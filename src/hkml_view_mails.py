19d0f84a455bfa53cc54379d627f32638c83cd01
SJ Park <sj@kernel.org>
2026-06-27 13:36:32 -0700

    hkml_view_mails: hint config for focused row display rule
    Signed-off-by: SJ Park <sj@kernel.org>

--- src/hkml_view_mails.py
+++ src/hkml_view_mails.py
@@ -1269,8 +1269,11 @@ def ask_focus_row_display_effect(mails_view_data, called_from_menu):
 
     desc = 'Select cursor-focused line display effect.'
     if called_from_menu is False:
-        desc += '\n(You can change this later using ' \
-                '"set focused row display effect" menu)'
+        desc += '\n- You can change this later using ' \
+                '"set focused row display effect" menu'
+        desc += '\n- You can also skip this question using\n' \
+                '  "hkml config set list-display-rule-focused-row ' \
+                '<reverse|color>"'
     _, selection, err = _hkml_cli.ask_selection(
             desc=desc, selections_txt=['No effect', 'reverse', 'color'],
             prompt='Select', default_selection_idx=0)
